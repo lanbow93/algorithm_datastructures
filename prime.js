feaffbdc2e596622815c04520e9663b1fabf09a7
lanbow93 <the.doctor1501@gmail.com>
2023-08-02 14:04:10 -0500

    added in case less than 2

--- prime.js
+++ prime.js
@@ -1,4 +1,7 @@
 function isPrime(n){
+    if(n<2){
+        return false
+    }
 	for(let i=2; i<(n/2); i++){
 		if(n%i === 0){                                                                              
 			return false                                                      
@@ -10,4 +13,4 @@ return true
 console.log(isPrime(3))
 console.log(isPrime(6))
 console.log(isPrime(9))
-console.log(isPrime(0))
+console.log(isPrime(-7))
