e1f405afe50b6f114f3c9df710a4586a861b3b50
lanbow93 <the.doctor1501@gmail.com>
2023-08-02 14:09:16 -0500

    optimized solution

--- prime.js
+++ prime.js
@@ -2,7 +2,7 @@ function isPrime(n){
     if(n<2){
         return false
     }
-	for(let i=2; i<(n/2); i++){
+	for(let i=2; i <= Math.sqrt(n); i++){ // If not prime, a number will have a factor less than or equal to its square root
 		if(n%i === 0){                                                                              
 			return false                                                      
 		}                                                                                                                                     
