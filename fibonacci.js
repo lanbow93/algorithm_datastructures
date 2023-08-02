70a6fbefebf606293f26b5c12763f6c2c3af5cff
lanbow93 <the.doctor1501@gmail.com>
2023-08-02 09:59:11 -0500

    fixed bug going past one

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,10 @@
 function fibonacci(number){
 	let targetArray = [0,1]
-	for (let i=1; i<number; i++){
+	for (let i=1; i<number-1; i++){
 		targetArray.push(targetArray[i] + targetArray[i-1] )
 }
 return targetArray 
 }
+
+
+console.log(fibonacci(2))
